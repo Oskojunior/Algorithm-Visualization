02fb6df820a6f5723723471125a839066180ed34
Osko <ia.kowalczyk5@gmail.com>
2022-04-07 22:14:19 +0200

    Setting up

---
+++ backend/src/main/java/com/example/algorithmvisualization/AlgorithmVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithmvisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmVisualizationApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/algorithmvisualization/AlgorithmVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithmvisualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmVisualizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
